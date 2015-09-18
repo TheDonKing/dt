1c7f6cc667d3a1fb12d65fb3cb190cd99e020466
TheDonKing <starspersona@gmail.com>
2015-09-18 15:56:25 +0300

    Модификация шаблона

--- index.php
+++ index.php
@@ -12,11 +12,14 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge"/>
     <meta name="author" lang="ru" content="s-dt.ru"/>
     <meta name="copyright" content="&copy; DTStudio <?php echo date("Y"); ?> "/>
-    <meta name="keywords" content="FraemWork"/>
-    <meta name="robots" content="all"/>
-    <meta name="description" content="Framework">
-    <title>Framework</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <meta name="keywords" content="создание сайтов, дизайн логотипа, фирменый стиль, логотип, бренбук, продвижение сайтов, дизайн интерьеров, дизайн экстерьеров">
+    <meta name="description" content="DTstudio - дизайн студия создание логотипов фирменого стиля, сайтов дизайна интерьера">
+    <title>Dtstudio - дизайн студия в Махачкале</title>
     <meta name='yandex-verification' content='6c33212f4dc14e06' />
+
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
     <link rel="icon" href="favicon.ico" type="image/x-icon">
@@ -36,7 +39,7 @@
     <!-- For iPad with high-resolution Retina display running iOS ≤ 7: -->
     <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png">
     <!-- Libs -->
-	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap.css" />
 	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css" />
@@ -51,22 +54,94 @@
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
-<div class="container">
-    <h1>Helllo world!</h1>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="logo col-md-5 col-sm-6" >
+                        <img src="img/logo.png" alt="Логотип DTstudio" />
+                        <p>твой дизайн</p>
+                    </div>
+                    <div class="number col-md-7 col-sm-6 ">
+                        <div class="col-md-offset-7 col-lg-offset-7 col-sm-offset-4">
+                            <p><a href="tel:+7 928 555-82-28">+7 963 555-82-28</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="services" >
+    <div class="container">
+        <div class="row">
+            <div class="services">
+                <div class="architecture col-md-4">
+                    <img src="img/services/architecture.jpg" alt="Архитектура и дизайн интерьера"/>
+                    <p><i class="sprite sprite-3"></i>Архитектура и дизайн интерьера</p>
+                </div>
+                <div class="web col-md-4">
+                    <img src="img/services/web.jpg" alt=""/>
+                    <p><i class="sprite sprite-2"></i>Сайты и продвижение</p>
+                </div>
+                <div class="logos col-md-4">
+                    <img src="img/services/logos.jpg" alt=""/>
+                    <p><i class="sprite sprite-1"></i>Логотипы и фирменные стили</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="container">
+        <div class="row">
+        <button id="inline" href="#form" class="fancybox  button button--quidel button--round-s"><span>Контакты</span></button>
+            <div style="display:none">
+                <div id="data">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+            </div>
+            <div class="social">
+                <div class="social-centered">
+                    <div>
+                        <a rel="nofollow" target="_blank"  href="https://instagram.com/dt_studio/"><i class="fa fa-instagram"></i></a>
+                        <a rel="nofollow" target="_blank" href="https://vk.com"><i class="fa fa-vk"></i></a>
+                        <a rel="nofollow" target="_blank" href="https://twitter.com"><i class="fa fa-twitter"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</footer>
+<div class="hidden">
+    <form id="form"  method="POST" class="cmxform" id="commentForm"  action="formdata.php">
+        <h3>Форма обратной связи:</h3>
+        <fieldset class="ui-widget ui-widget-content ui-corner-all">
+            <div class="icon name"><input  id="cname"  name="name" class="ui-widget-content" minlength="2"  type="text" placeholder="Имя" required=""/></div><br />
+            <div class="icon mail"><input   id="cemail" name="mail"  class="ui-widget-content" type="email" placeholder="Email" required="" /></div><br />
+            <div class="icon phone"><input  id="cdate"  name="phone" class="ui-widget-content"  type="phone" placeholder="Номер" required="" /></div><br />
+            <button  class="btn btn-primary"  type="submit">Отправить</button>
+        </fieldset>
+    </form>
 </div>
 
 <!-- Тут подключаем скрипты -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="libs/waypoints/waypoints-1.6.2.min.js"></script>
-	<script src="libs/scrollto/jquery.scrollTo.min.js"></script>
-	<script src="libs/owl-carousel/owl.carousel.min.js"></script>
-	<script src="libs/countdown/jquery.plugin.js"></script>
-	<script src="libs/countdown/jquery.countdown.min.js"></script>
-	<script src="libs/countdown/jquery.countdown-ru.js"></script>
-	<script src="libs/landing-nav/navigation.js"></script>
-	<script src="js/common.js"></script>
+    <script type="text/javascript" src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script type="text/javascript" src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="libs/waypoints/waypoints-1.6.2.min.js"></script>
+	<script type="text/javascript" src="libs/scrollto/jquery.scrollTo.min.js"></script>
+	<script type="text/javascript" src="libs/owl-carousel/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="libs/countdown/jquery.plugin.js"></script>
+	<script type="text/javascript" src="libs/countdown/jquery.countdown.min.js"></script>
+	<script type="text/javascript" src="libs/countdown/jquery.countdown-ru.js"></script>
+	<script type="text/javascript" src="libs/landing-nav/navigation.js"></script>
+    <script type="text/javascript" src="libs/bootstrap/bootstrap.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.js"></script>
+	<script type="text/javascript" src="js/common.js"></script>
 	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
 	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
 </body>
